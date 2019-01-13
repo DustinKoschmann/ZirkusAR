630e152fa1893672baced5ed35522eabdb61a6ec
LukasWis <lukas.wistuba.studi@gmail.com>
2019-01-13 13:28:58 +0100

    GUI Balance

--- Zirkus_AR/Assets/BalanceController.cs
+++ Zirkus_AR/Assets/BalanceController.cs
@@ -5,13 +5,20 @@ using UnityEngine.UI;
 
 public class BalanceController : MonoBehaviour {
 
+	public GameObject artist;
+	public GameObject finish;
+	
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Image>().color = new Color32(220,111,111,255);
+		if (artist.transform.position.z == finish.transform.position.z) {
+			
+			GetComponent<Image>().color = new Color32(220,111,111,255);
+			
+		}
 	}
 }
