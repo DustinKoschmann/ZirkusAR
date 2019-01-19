5f13d6215e5d8fb6cd20ad4813251d5b44589d37
LukasWis <lukas.wistuba.studi@gmail.com>
2019-01-19 11:31:26 +0100

    added menu (anleitung)
    bei klick auf burger menu erscheint eine kurze anleitung

---
+++ Zirkus_AR/Assets/menuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuController : MonoBehaviour {
+
+	public GameObject anleitung;
+	private bool isOpen;
+
+
+	// Use this for initialization
+	void Start () {
+		isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void showMenu () {
+		
+		if (isOpen == false) {
+			anleitung.SetActive(true);
+			isOpen = true;
+		} else {
+			anleitung.SetActive(false);
+			isOpen = false;
+		}
+		
+	}
+}
