0c30960c3f4f096000be31fc31f1a496e0253c20
LukasWis <lukas.wistuba.studi@gmail.com>
2019-01-19 12:09:25 +0100

    added "check" icon after finishing a station
    wenn station erledigt, wird icon eingefärbt und hintergrund mit check-icon für 3 sek

--- Zirkus_AR/Assets/BalanceController.cs
+++ Zirkus_AR/Assets/BalanceController.cs
@@ -7,20 +7,31 @@ public class BalanceController : MonoBehaviour {
 
 	public GameObject artist;
 	public GameObject finish;
+	public GameObject check;
     private MovementBalance movementBalance;
+	private bool done;
 	
 	// Use this for initialization
 	void Start () {
-
+		done = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         if (artist.transform.position.z == finish.transform.position.z || artist.GetComponent<MovementBalance>().zielErreicht == true) {
-			
-			GetComponent<Image>().color = new Color32(220,111,111,255);
-			
+			if (done == false) {
+				done = true;
+				GetComponent<Image>().color = new Color32(220,111,111,255);
+				StartCoroutine(isDone());
+			}
 		}
 	}
+	
+	IEnumerator isDone() {
+		yield return new WaitForSeconds(0.5f);
+		check.SetActive(true);
+		yield return new WaitForSeconds(3);
+        check.SetActive(false);
+    }
 }
--- Zirkus_AR/Assets/DosenController.cs
+++ Zirkus_AR/Assets/DosenController.cs
@@ -6,19 +6,24 @@ using UnityEngine.UI;
 public class DosenController : MonoBehaviour {
 
 	public GameObject[] dosen;
+	public GameObject check;
 	private int counter;
+	private bool done;
 
 	// Use this for initialization
 	void Start () {
 		counter = 0;
+		done = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		// Wenn counter auf 6, dann färbe ein
-		if (dosen.Length == counter) {
+		if (dosen.Length == counter && done == false) {
+			done = true;
 			GetComponent<Image>().color = new Color32(220,111,111,255);
+			StartCoroutine(isDone());
 		}
 		
 		//Debug.Log (counter);
@@ -35,4 +40,11 @@ public class DosenController : MonoBehaviour {
         }
 		
 	}
+	
+	IEnumerator isDone() {
+		yield return new WaitForSeconds(0.5f);
+		check.SetActive(true);
+		yield return new WaitForSeconds(3);
+        check.SetActive(false);
+    }
 }
