2328155289448160132ec70f86368002e0629f90
LukasWis <lukas.wistuba.studi@gmail.com>
2019-01-21 19:16:31 +0100

    update

--- Zirkus_AR/Assets/BalanceController.cs
+++ Zirkus_AR/Assets/BalanceController.cs
@@ -13,7 +13,6 @@ public class BalanceController : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		GetComponent<Image>().color = new Color32(220,111,111,255);
 		done = false;
 	}
 	
