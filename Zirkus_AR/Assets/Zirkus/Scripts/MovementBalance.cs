282d7fa383e83787721f8460da33083d0885f15d
DustinKoschmann <Dustin.Koschmann@web.de>
2019-02-05 17:13:49 +0100

    Alte ImageTargets
    - Doch wieder alte ImageTargets
    - Balance reset verbessert

--- Zirkus_AR/Assets/Zirkus/Scripts/MovementBalance.cs
+++ Zirkus_AR/Assets/Zirkus/Scripts/MovementBalance.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementBalance : MonoBehaviour {
+    public bool zielErreicht = false;
+
+    public Transform startPosition;
+
+
     private bool isWalking = false;
     private bool firedOnce = false;
-    public  bool zielErreicht = false;
     private bool isBalanced = false;
     private float speed = 0.1f;
     private float horizontalMovement;
     private int actualTarget;
-    public Transform startPosition;
+    private bool startedWalking = false;
 
     private Transform parentTarget;
     private Transform[] targets;
@@ -19,13 +23,52 @@ public class MovementBalance : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        actualTarget = 0;
         animator = this.transform.GetComponent<Animator>();
         boneToRotate = this.transform.Find("metarig/hips/spine");
+        resetPosition();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!firedOnce) {
+            try {
+                parentTarget = GameObject.Find("Targets").transform;
+            } catch { }
+
+            if (parentTarget != null) {
+                firedOnce = true;
+            }
+
+            Transform[] children = {
+                parentTarget.GetChild(0),
+                parentTarget.GetChild(1),
+                parentTarget.GetChild(2),
+                parentTarget.GetChild(3),
+                parentTarget.GetChild(4)
+            };
+
+            targets = children;
+            //Debug.Log(targets.Length);
+        } else {
+            if (actualTarget >= targets.Length - 1) {
+                zielErreicht = true;
+            }
+
+            if ((transform.position == targets[actualTarget].position) && !zielErreicht) {
+                actualTarget++;
+            }
+
+            if (!zielErreicht && isBalanced && startedWalking) {
+                isWalking = true;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, targets[actualTarget].position, step);
+            } else {
+                isWalking = false;
+            }
+            animator.SetBool("isWalking", isWalking);
+            BalanceBone();
+        }
+
         if (Input.touchCount > 0) {
             var touch = Input.touches[0];
             if (touch.position.x < Screen.width/2) {
@@ -47,49 +90,16 @@ public class MovementBalance : MonoBehaviour {
             horizontalMovement = 0;
         }
 
-        if (!firedOnce) {
-            try {
-                parentTarget = GameObject.Find("Targets").transform;
-            } catch { }      
-            
-            if(parentTarget != null) {
-                firedOnce = true;
-            }
-
-            Transform[] children = {
-                parentTarget.GetChild(0),
-                parentTarget.GetChild(1),
-                parentTarget.GetChild(2),
-                parentTarget.GetChild(3),
-                parentTarget.GetChild(4)
-            };
-
-            targets = children;
-            //Debug.Log(targets.Length);
-        } else {
-            if (actualTarget >= targets.Length - 1) {
-                zielErreicht = true;
-            }
-
-            if ((transform.position == targets[actualTarget].position) && !zielErreicht) {
-                actualTarget++;
-            }            
-
-            if (!zielErreicht && isBalanced) {
-                isWalking = true;
-                float step = speed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, targets[actualTarget].position, step);
-            } else {
-                isWalking = false;
-            }
-            animator.SetBool("isWalking", isWalking);
-            BalanceBone();
-        }
+        if(horizontalMovement != 0) {
+            startedWalking = true;
+        }        
 	}
 
     void resetPosition() {
+        startedWalking = false;
         actualTarget = 0;
         transform.position = startPosition.transform.position;
+        boneToRotate.localEulerAngles = new Vector3(0, 0, 0f);
     }
 
     void BalanceBone() {
@@ -114,7 +124,6 @@ public class MovementBalance : MonoBehaviour {
 
                 //Wenn zu weit nach links, nicht weiter rotieren
                 if (boneRot.z > 100 && boneRot.z < 180) {
-                    boneToRotate.localEulerAngles = new Vector3(0, 0, 1f);
                     resetPosition();
                 }
 
@@ -125,7 +134,6 @@ public class MovementBalance : MonoBehaviour {
 
                 //Wenn zu weit nach rechts, nicht weiter rotieren
                 if (boneRot.z < 260 && boneRot.z > 180) {
-                    boneToRotate.localEulerAngles = new Vector3(0, 0, 1f);
                     resetPosition();
                 }
             }
